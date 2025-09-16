376939b1734c00f67672d2ecbe7e10b89f4e50a1
deepan <deepanappu1999@gmail.com>
2025-09-16 20:07:59 +0530

    setting up the express, Mongo DB

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const port = 4500
+
+
+
+
+
+
+app.get('/',(req,res) => {
+    if(req.url === '/') {
+        res.send("Hello Express");
+        res.end()
+    }
+})
+
+
+app.listen(port,() => {
+    console.log(`Application is running on ${port}`)
+})
+mongoose.connect('mongodb://127.0.0.1/Vidly')
+.then(() => console.log('Connected to Vidly DB'))
+.catch((err) => console.error('Error',err.message))
