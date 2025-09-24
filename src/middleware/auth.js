332adb26ca474b6095eaaa96727860685d1aba78
deepan <deepanappu1999@gmail.com>
2025-09-24 21:54:29 +0530

    checking for invalid token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 import config from 'config'
 
-export  default function auth(req, res, next) {
+export  default async function auth(req, res, next) {
     const token = req.header('x-auth-token');
     if (!token) return res.status(401).send('Acess denied. No token provided')
 
     try {
-        const {decode} = jwtVerify(token, config.get('jwtPrivateKey'))
+        const decode = await jwtVerify(token, config.get('jwtPrivateKey'))
         req.user = decode
         next()
     }
