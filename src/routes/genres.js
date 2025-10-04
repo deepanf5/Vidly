09a0da36712ec57c56ce219fdfc17fb9268965e6
deepan <deepanappu1999@gmail.com>
2025-10-04 12:29:00 +0530

    removing the error

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -24,8 +24,6 @@ export const Genres = mongoose.model('Genres', GenreSchema)
 
 
 router.get('/', asyncmiddleWare(async (req, res, next) => {
-
-    throw new Error('not accessible ')
     const genres = await Genres.find().sort('name')
     res.send(genres);
 }));
